615fee32cbf1ffe725d25be10ec1ff2a40ffe8d2
mohakkapoor4 <mkapoor@humanizeiq.ai>
2026-02-09 21:24:07 +0530

    feat: Add script to fetch financial data for Nifty50, Gold, and USDINR, and create an initial exploration notebook.

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -2,12 +2,12 @@ import polars as pl
 import numpy as np  
 import yfinance as yf   
 
-tickers = ['^NSEI','GOLDBEES.NS','USDINR=X']
+tickers = {"Nifty50":"^NSEI","Gold":"GOLDBEES.NS","USDINR":"USDINR=X"}
 
-for tick in tickers:
+for name,tick in tickers.items():
     datH = yf.download(tick, start='2024-3-01', end='2026-01-01',interval='1h')
-    datH.to_parquet(f'data/raw/{tick}_1h.parquet')
-    print(f'{tick} hourly data collected and saved')
+    datH.to_parquet(f'data/raw/{name}_1h.parquet')
+    print(f'{name} hourly data collected and saved')
     datD = yf.download(tick, start='2014-01-01', end='2026-01-01',interval='1d') 
-    datD.to_parquet(f'data/raw/{tick}_1d.parquet')
-    print(f'{tick} daily data collected and saved')
+    datD.to_parquet(f'data/raw/{name}_1d.parquet')
+    print(f'{name} daily data collected and saved')
