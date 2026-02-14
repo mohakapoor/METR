135deaf6b726c3cf78f1c87059c489481bcb2123
mohakkapoor4 <mkapoor@humanizeiq.ai>
2026-02-14 16:31:48 +0530

    feat: Implement data fetching for financial assets and an initial exploration notebook for loading and combining daily closing prices.

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -5,9 +5,9 @@ import yfinance as yf
 tickers = {"Nifty50":"^NSEI","Gold":"GOLDBEES.NS","USDINR":"USDINR=X"}
 
 for name,tick in tickers.items():
-    datH = yf.download(tick, start='2024-3-01', end='2026-01-01',interval='1h')
-    datH.to_parquet(f'data/raw/{name}_1h.parquet')
-    print(f'{name} hourly data collected and saved')
-    datD = yf.download(tick, start='2014-01-01', end='2026-01-01',interval='1d') 
+    # datH = yf.download(tick, start='2024-3-01', end='2026-01-01',interval='1h')
+    # datH.to_parquet(f'data/raw/{name}_1h.parquet')
+    # print(f'{name} hourly data collected and saved')
+    datD = yf.download(tick, start='2013-01-01', end='2026-01-01',interval='1d') 
     datD.to_parquet(f'data/raw/{name}_1d.parquet')
     print(f'{name} daily data collected and saved')
