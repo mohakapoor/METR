ceb1f1fc158323e35e47ee403ac36a91fc1a2ea4
mohakkapoor4 <mkapoor@humanizeiq.ai>
2026-02-15 19:42:35 +0530

    feat: implementing hyper parameter tuning and get high accuracy

--- src/train_exposure.py
+++ src/train_exposure.py
@@ -3,21 +3,15 @@ import xgboost as xgb
 import numpy as np
 import yaml
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import TimeSeriesSplit
-
-CONFIG_PATH = "config.yaml"
+from sklearn.model_selection import TimeSeriesSplit,GridSearchCV
 
 
+train = pl.read_parquet("nifty_train.parquet")
+test = pl.read_parquet("nifty_test.parquet")
 
-train = pl.read_parquet("data/processed/train/nifty.parquet")
-test = pl.read_parquet("data/processed/test/nifty.parquet")
 
+exp_feat = ["Ret_1d","Ret_3d","Ret_5d","Ret_20d","Vol_5d","Vol_20d","Vol_Ratio","MA_Ratio","Close_Pos_Range","Intraday_Return","Price_vs_MA20",'MACD_Hist','RSI','ROC_10','BB_Pct',    "ATR_Pct","Ret_1d_Lag1","Ret_1d_Lag2","Ret_1d_Lag3"]
 
-with open(CONFIG_PATH,"r") as f:
-    config = yaml.safe_load(f)
-
-exp_feat = config["Exposure_Features"]
-splits = config["N_Splits"]
 
 X_train = train.select(exp_feat).to_pandas()
 y_train = train["Label"].to_pandas()
@@ -25,36 +19,43 @@ y_train = train["Label"].to_pandas()
 X_test = test.select(exp_feat).to_pandas()
 y_test = test["Label"].to_pandas()
 
-tscv = TimeSeriesSplit(n_splits = splits)
 
 
 
-model = xgb.XGBClassifier(
-    max_depth = 3,
-    random_state = 42,
-    n_estimators = 100,
+
+param_grid = {
+    'max_depth' : [2,3,4,5],
+    'learning_rate' : [0.01,0.03,0.05,0.1],
+    'n_estimators' : [100,150,200,250,300],
+    'subsample' : [1.0]
+}
+
+tscv = TimeSeriesSplit(n_splits = 5)
+
+search = GridSearchCV(
+    estimator=xgb.XGBClassifier(random_state=42, device='cuda'),  # Use GPU
+    param_grid=param_grid,
+    scoring = 'roc_auc',
+    cv =tscv,
+    verbose =3,
+    n_jobs = 1, # <--- Limit CPU threads; GPU does the work
 )
 
-for fold,(train_index,val_index) in enumerate(tscv.split(X_train)):
-    X_t,X_v = X_train.iloc[train_index],X_train.iloc[val_index]
-    y_t,y_v = y_train.iloc[train_index],y_train.iloc[val_index]
 
-    model = xgb.XGBClassifier(
-        max_depth = 3,
-        random_state = 42,
-        n_estimators = 100,
-    )
-
-    model.fit(X_t,y_t)
-
-    preds = model.predict(X_v)
+search.fit(X_train, y_train)
+print(f"Best Score: {search.best_score_:.4f}")
+print(f"Best Params: {search.best_params_}")
+# 5. Use the Best Model for Final Test
+best_model = search.best_estimator_
+preds = best_model.predict(X_test)
 
 
-    accuracy = accuracy_score(y_v, preds)
-    print(f"Test Accuracy: {accuracy:.4f}")
+accuracy = accuracy_score(y_test, preds)
+print(f"Test Accuracy: {accuracy:.4f}")
 
-    print("\nClassification Report:")
-    print(classification_report(y_v, preds))
+print("\nClassification Report:")
+print(classification_report(y_test, preds))
+
 
 
 
