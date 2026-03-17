d2e2199fe23491a64c1550cefb6b903b763d4a81
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-03-18 01:36:52 +0530

    Triple barier labelling

---
+++ src/tripple_barrier.py
@@ -0,0 +1,36 @@
+import polars as pl
+import numpy as np
+
+
+def generate_barriers(df,k,T):
+    labels = []
+    opens = df['Open'].to_list()
+    high = df['High'].to_list()
+    low = df['Low'].to_list()
+    vol_20d = df['Vol_20d'].to_list()
+    for i in range(len(opens)-T-1):
+        label = 0 # base case that T days passed without anything
+        p0 = opens[i+1]
+        sigma = vol_20d[i]
+        p_upper = p0*(1+k*sigma)
+        p_lower = p0*(1-k*sigma)
+
+        for j in range(1,T+1):
+            if(high[i+j] >= p_upper) and (low[i+j] <= p_lower):
+                label = -1 # assuming low was first to be safe
+                break
+            elif(high[i+j] >= p_upper):
+                label = 1 # profit
+                break
+            elif(low[i+j] <= p_lower):
+                label = -1 # stop loss
+                break
+            else:
+                pass
+        labels.append(label)
+    df['Label'] = labels
+    return df
+
+
+df = pl.read_parquet(r"data\processed\train\nifty.parquet")
+
