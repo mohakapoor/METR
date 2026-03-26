1a1d8a0900a7e6999f7fc2867141bc3160aa35d1
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-03-27 00:19:22 +0530

    add best_d_value function

--- src/frac_diff.py
+++ src/frac_diff.py
@@ -23,31 +23,46 @@ def frac_diff(series, d, thresh=1e-5):
     
     return out
 
+def best_d_value(d_values,pvals):
+    if len(d_values) == len(pvals):
+        scores = sorted(zip(d_values,pvals))
+        for i,j in scores:
+            if(j<0.05):
+                return i
+
 def evaluate_d(assets):
     for name, df in assets:
         series = np.array(df['Close'])
         d_values = np.arange(0.1,1.0,0.1)
         pvals = []
-        for d in d_values:
-            fd_series = frac_diff(series, d)
-            fd_series = fd_series[~np.isnan(fd_series)]
-            if len(fd_series) < 20:
-                pvals.append(np.nan)
-                continue
-            try:
-                pval = adfuller(fd_series)[1]
-            except:
-                pval = np.nan
-            print(f"d={d:.2f}, p-value={pval}, asset = {name}")
-            pvals.append(pval)
+        with open(f"reports/tripple_barrier/{name}_grid_results.txt", "w", encoding="utf-8") as f:
+            f.write(name.upper() + ":\n")
+            for d in d_values:
+                fd_series = frac_diff(series, d)
+                fd_series = fd_series[~np.isnan(fd_series)]
+                if len(fd_series) < 20:
+                    pvals.append(np.nan)
+                    continue
+                try:
+                    pval = adfuller(fd_series)[1]
+                except:
+                    pval = np.nan
+                f.write(f"d={d:.2f}, p-value={pval} \n")
+                print(f"d={d:.2f}, p-value={pval}, asset = {name}")
+                pvals.append(pval)
+            best_d = best_d_value(d_values,pvals)
+
+        
 
         plt.figure()
         plt.plot(d_values, pvals, marker='o')
         plt.axhline(0.05)  # threshold line
         plt.title(f"ADF p-value vs d ({name})")
         plt.xlabel("d")
-        plt.ylabel("p-value")
+        plt.ylabel("p-value  (log)")
+        plt.yscale('log')
         plt.show()
+    
 
 
 
