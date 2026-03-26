8b52a5a45d028da7210bdb1666341c951a24c87d
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-03-27 01:04:00 +0530

    update fetch_data to include indiavix too

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -13,3 +13,6 @@ for name,tick in tickers.items():
     datD = yf.download(tick, start='2013-01-01', end='2026-02-01',interval='1d') 
     datD.to_parquet(f'data/raw/{name}_1d.parquet')
     print(f'{name} daily data collected and saved')
+
+vix = yf.download("^INDIAVIX", start="2013-01-01", end="2025-12-31")
+vix.to_parquet(f'data/raw/India_VIX.parquet')
