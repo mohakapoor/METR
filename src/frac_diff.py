26a1271be23ccd2ea5f5697b9c7584e2ec960650
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-03-26 19:37:42 +0530

    adf test

--- src/frac_diff.py
+++ src/frac_diff.py
@@ -1,4 +1,6 @@
 import numpy as np
+import polars as pl
+import numpy as np
 from statsmodels.tsa.stattools import adfuller
 
 def get_weights(d, size):
@@ -21,15 +23,17 @@ def frac_diff(series, d, thresh=1e-5):
     
     return out
 
-
-
-
-for d in np.arange(0.1, 1.0, 0.1):
-    fd_series = frac_diff(series.values, d)
-    fd_series = fd_series[~np.isnan(fd_series)]
+def evaluate_d(assets):
+    for name, df in assets:
+        series = np.array(df['Close'])
     
-    pval = adfuller(fd_series)[1]
-    print(f"d={d:.2f}, p-value={pval}")
+        for d in np.arange(0.1, 1.0, 0.1):
+            fd_series = frac_diff(series, d)
+            fd_series = fd_series[~np.isnan(fd_series)]
+            if len(fd_series) < 20:
+                continue
+            pval = adfuller(fd_series)[1]
+            print(f"d={d:.2f}, p-value={pval}, asset = {name}")
 
 
 
@@ -43,3 +47,4 @@ if __name__ == "__main__":
         ("gold", df2),
         ("usdinr", df3),
     ]
+    evaluate_d(assets=assets)
